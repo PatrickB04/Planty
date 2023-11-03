bad99ed77730c54e23baf00348a45032fcb68d76
PatrickB04 <113237775+PatrickB04@users.noreply.github.com>
2023-11-03 19:13:43 +0100

    Travail sur le responsive design
    adaptation de l'affichage mobile et tablette

--- wp-content/themes/2023-child/functions.php
+++ wp-content/themes/2023-child/functions.php
@@ -6,25 +6,13 @@ function theme_enqueue_styles()
     wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/custom-style.css', array(), filemtime(get_stylesheet_directory() . '/custom-style.css'));
 }
 
-function masquer_element_admin_non_connecte($items, $args) {
-    if ($args->theme_location == 'nav-menu') {
-        if (is_user_logged_in()) {
-            $items .= '<li><a href="#">admin</a></li>';
-        }
-    }
-    return $items;
+function remove_menu_item(){
+	if (!is_user_logged_in()) {?>
+	<script>
+		 document.querySelectorAll('.wp-block-group .wp-block-navigation-item')[1].remove()
+	</script>
+<?php }
 }
-add_filter('wp_nav_menu_items', 'masquer_element_admin_non_connecte', 10, 2);
-
-function add_login_logout_link_to_menu( $items, $args ) {
-    if (is_user_logged_in()) {
-        $items .= '<li><a href="' . wp_logout_url() . '">Déconnexion</a></li>';
-    } else {
-        $items .= '<li><a href="' . wp_login_url() . '">Connexion</a></li>';
-    }
-    return $items;
-}
-add_filter( 'wp_nav_menu_items', 'add_login_logout_link_to_menu', 10, 2 );
-
+add_action('wp_footer', 'remove_menu_item');
 
 ?>
